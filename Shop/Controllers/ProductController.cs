09b26efdd3905cbe9170d66d04b752d73498e6aa
Yuri <erokhin93@yandex.ru>
2018-07-05 10:56:38 +0500

    Added files

---
+++ Shop/Controllers/ProductController.cs
@@ -0,0 +1,94 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(string title, int price)
+        {
+            AddProduct(new Product { Title = title, Price = price }).Wait();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private async Task<Product> AddProduct(Product product)
+        {
+            using (var db = new ProductContext())
+            {
+                db.Products.Add(product);
+                await db.SaveChangesAsync();
+            }
+            return product;
+        }
+
+        public ActionResult Delete(int id)
+        {
+            DeleteProduct(id).Wait();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost, ActionName("Delete")]
+        private async Task<int> DeleteProduct(int id)
+        {
+            using (var db = new ProductContext())
+            {
+                Product product = await db.Products.FindAsync(id);
+                db.Products.Remove(product);
+                await db.SaveChangesAsync();
+                return id;
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            Product product = GetProduct(id).Result;
+            return View(product);
+        }
+
+        private async Task<Product> GetProduct(int id)
+        {
+            using (var db = new ProductContext())
+            {
+                Product product = await db.Products.FindAsync(id);
+                return product;
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Product product = GetProduct(id).Result;
+            return View(product);
+        }
+
+        public ActionResult EditProduct([Bind(Include = "Id,Title,Price")] Product product)
+        {
+            EditProductAction(product).Wait();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<Product> EditProductAction(Product product)
+        {
+            using (var db = new ProductContext())
+            {
+                db.Entry(product).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return product;
+            }
+        }
+    }
+}
---
+++ Shop/Models/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("conn") { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
